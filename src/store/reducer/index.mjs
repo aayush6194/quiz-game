f7abd92c91142ecb76f96b263e23563087f27758
Lawrence Khadka <l.khadka@outlook.com>
2020-10-25 21:45:12 -0600

    add to main reducer

--- src/store/reducer/index.mjs
+++ src/store/reducer/index.mjs
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 // import rooms from './room.mjs';
 import quiz from './quiz.mjs';
+import room from './room.mjs';
+import vote from './vote.mjs';
 
 export default combineReducers({
     quiz,
+    room,
+    vote,
 });
