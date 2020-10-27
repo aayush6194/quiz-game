e648241022fc530d845fe34f7157c977cd83a4d3
Lawrence Khadka <l.khadka@outlook.com>
2020-10-27 13:26:37 -0600

    add timeout for quiz

--- src/store/index.mjs
+++ src/store/index.mjs
@@ -1,6 +1,13 @@
-import Redux from 'redux';
+import Redux, { applyMiddleware } from 'redux';
+import saga from 'redux-saga';
 import rootReducer from './reducer/reducer.mjs';
+import rootSaga from './sagas.mjs';
 
-const store = Redux.createStore(rootReducer);
+const { default: createSagaMiddleware } = saga;
+const sagaMiddleware = createSagaMiddleware();
+
+const store = Redux.createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
