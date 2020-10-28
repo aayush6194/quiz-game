2206354b22078d31a23547af7bdb63fc38a853dd
Lawrence Khadka <l.khadka@outlook.com>
2020-10-28 00:57:47 -0600

    remove unnecessary actions to client

--- src/socket.mjs
+++ src/socket.mjs
@@ -1,32 +1,26 @@
 import WebSocket from 'ws';
-import store from './store/index.mjs';
 import { mapClientActions } from './store/actions/helpers.mjs';
+import store from './store/index.mjs';
 
 const wss = new WebSocket.Server({ noServer: true });
 
-store.subscribe(() => {
-    wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(
-                JSON.stringify({
-                    type: 'LOAD_STATE',
-                    payload: store.getState(),
-                })
-            );
-        }
-    });
-});
-
 wss.on('connection', (socket) => {
-    socket.send(
-        JSON.stringify({ type: 'LOAD_STATE', payload: store.getState() })
-    );
     socket.on('message', (data) => {
         const action = JSON.parse(data);
-        store.dispatch.bind(store)({
-            type: mapClientActions(action.type),
-            payload: action.payload,
-        });
+        if (action.type === 'CREATE_PLAYER') {
+            store.dispatch.bind(store)({
+                type: action.type,
+                payload: {
+                    ...action.payload,
+                    socket,
+                },
+            });
+        } else {
+            store.dispatch.bind(store)({
+                type: mapClientActions(action.type),
+                payload: action.payload,
+            });
+        }
     });
 });
 
