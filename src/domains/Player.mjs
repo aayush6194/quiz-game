4eb351d99821cf408ae27ebef98101b7f3ce6f10
Lawrence Khadka <l.khadka@outlook.com>
2020-10-27 20:35:22 -0600

    make it pure

--- src/domains/Player.mjs
+++ src/domains/Player.mjs
@@ -1,9 +1,6 @@
 import R from 'rambda';
-import udid from 'uuid';
 
-const { v1 } = udid;
-
-export const Player = R.curry(({ name, avatar, socket, id = v1() }) =>
+export const Player = R.curry(({ id, name, avatar, socket }) =>
     R.compose(
         Object.seal,
         Object.freeze
