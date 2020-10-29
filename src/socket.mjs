b6cdbfe13d52bb22ae17570b785962f0251d2150
Lawrence Khadka <l.khadka@outlook.com>
2020-10-28 21:12:01 -0600

    removed unused

--- src/socket.mjs
+++ src/socket.mjs
@@ -1,5 +1,4 @@
 import WebSocket from 'ws';
-import { mapClientActions } from './store/actions/helpers.mjs';
 import store from './store/index.mjs';
 
 const wss = new WebSocket.Server({ noServer: true });
@@ -16,10 +15,7 @@ wss.on('connection', (socket) => {
                 },
             });
         } else {
-            store.dispatch.bind(store)({
-                type: mapClientActions(action.type),
-                payload: action.payload,
-            });
+            store.dispatch.bind(store)(action);
         }
     });
     socket.on('close', () => {
